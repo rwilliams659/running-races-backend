27da83ba1389668fed5c27705061dc399692b06a
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 17:44:16 -0600

    Add dataset to serve.js file

---
+++ server.js
@@ -0,0 +1,44 @@
+const marathons = [
+  {
+    id: 1,
+    name: 'New York City Marathon',
+    location: 'New York, New York',
+    maxRunners: 53500,
+    date: '10-17-2020'
+  },
+  {
+    id: 2,
+    name: 'Boston Marathon',
+    location: 'Boston, MA',
+    maxRunners: 31500,
+    date: '9-14-2020'
+  },
+  {
+    id: 3,
+    name: 'Philadelphia Marathon',
+    location: 'Philadelphia, PA',
+    maxRunners: 15000,
+    date: '11-24-2020'
+  },
+  {
+    id: 4,
+    name: 'Walt Disney World Marathon',
+    location: 'Orlando, FL',
+    maxRunners: 15000,
+    date: '1-10-2021'
+  },
+  {
+    id: 5,
+    name: 'Los Angeles Marathon',
+    location: 'Los Angeles, CA',
+    maxRunners: 20000,
+    date: '3-21-2021'
+  },
+  {
+    id: 6,
+    name: 'Big Sur International Marathon',
+    location: 'Big Sur, CA',
+    maxRunners: 4500,
+    date: '4-25-2021'
+  },
+]
