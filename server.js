25c9bfc8ebc3a28473aecdd1325f658277aaef1d
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 18:56:16 -0600

    Add post endpoint for adding new marathon

--- server.js
+++ server.js
@@ -24,3 +24,19 @@ app.get('/api/v1/marathons/:id', (request, response) => {
   }
   response.status(200).json({requestedRace})
 })
+
+app.post('/api/v1/marathons', (request, response) => {
+  const requiredProperties = ['name', 'location', 'maxRunners', 'date'];
+  for (let property of requiredProperties) {
+    if(!request.body[property]) {
+      return response.status(422).json({error: `Unsuccessful POST. Property of ${property} must be included.`})
+    }
+  };
+  const { name, location, maxRunners, date } = request.body; 
+  const id = Date.now(); 
+  const newMarathon = { id, name, location, maxRunners, date }
+  app.locals.marathons.push(newMarathon);
+  response.status(201).json(newMarathon);
+})
+
+// One POST endpoint to create new objects in your data set.This endpoint should check every property in the resource being passed in to make sure that all of the properties required to make a new resource were provided.If the user does not provide those required properties, you should send back an error with a message detailing why their request could not be completed.
