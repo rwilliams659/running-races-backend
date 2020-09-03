ab4a7aa728580f7718ead815d1bca52d700ab583
Rachel Williams <rachelwilliams659@gmail.com>
2020-09-02 18:35:50 -0600

    Add get endpoints to retrieve all and single marathons

--- server.js
+++ server.js
@@ -5,6 +5,22 @@ app.use(express.json());
 app.set('port', 3002); 
 
 app.locals.title='Top US Marathons'
-app.locals.marathons = marathons; 
+app.locals.marathons = marathons.marathons; 
 
-//Using Express, build a server that holds your data in app.locals. This server should spin up to a port of your choice on localhost and return a greeting message in your terminal when the server is started.
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is up and running on http://localhost:${app.get('port')}.`);
+});
+
+app.get('/api/v1/marathons', (request, response) => {
+  response.status(200).json({marathons: app.locals.marathons})
+})
+
+
+app.get('/api/v1/marathons/:id', (request, response) => {
+  const id = +request.params.id;
+  const requestedRace = app.locals.marathons.find(marathon => marathon.id === id);
+  if (!requestedRace) {
+    response.status(404).json({error: `Sorry, we couldn't find a race with an id of ${id}`})
+  }
+  response.status(200).json({requestedRace})
+})
